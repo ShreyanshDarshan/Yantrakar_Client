d2a5d89a9176f803b3efc1a92977c854b90944db
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2020-07-26 15:49:05 +0530

    update viewAll

--- viewALL.py
+++ viewALL.py
@@ -1,27 +1,33 @@
 import mysql.connector as mysql
+import time
 
 passFile = open("pass.txt","r")
 mysql_pass = passFile.readline()
 passFile.close()
 
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = mysql_pass,
-    database = "test"
-)
 
-cursor = db.cursor()
+while (True):
+    db = mysql.connect(
+        host = "localhost",
+        user = "root",
+        passwd = mysql_pass,
+        database = "test"
+    )
 
-## defining the Query
-query = "SELECT * FROM cameradatabasefinal"
+    cursor = db.cursor()
+    print ("getting data")
+    ## defining the Query
+    query = "SELECT * FROM cameradatabasefinal"
 
-## getting records from the table
-cursor.execute(query)
+    ## getting records from the table
+    cursor.execute(query)
 
-## fetching all records from the 'cursor' object
-records = cursor.fetchall()
+    ## fetching all records from the 'cursor' object
+    records = cursor.fetchall()
 
-## Showing the data
-for record in records:
-    print(record)
+    ## Showing the data
+    for record in records:
+        print(record)
+    
+    print (len(records))
+    time.sleep(1)
