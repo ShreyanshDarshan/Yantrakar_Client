dfdff75c313061f4bb736402db8ae29bd1eeec0f
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2020-07-26 15:16:49 +0530

    add password file to DB management pyscripts

--- createDB.py
+++ createDB.py
@@ -1,9 +1,13 @@
 import mysql.connector as mysql
 
+passFile = open("pass.txt","r")
+mysql_pass = passFile.readline()
+passFile.close()
+
 db = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = "darshan_sql"
+    passwd = mysql_pass
 )
 
 ## creating an instance of 'cursor' class which is used to execute the 'SQL' statements in 'Python'
