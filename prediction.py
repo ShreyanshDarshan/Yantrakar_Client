242ce60a63b08ef67249d33e991730ddfeaca491
ShrinivasK100 <shrinivas khistesk@gmail.com>
2020-07-28 02:20:46 +0530

    Merged Prediction and Tranformation

--- prediction.py
+++ prediction.py
@@ -12,38 +12,40 @@ import _thread
 import threading 
 import gzip
 import transformation
+from itertools import combinations
 import os
 
-passFile = open("pass.txt","r")
-mysql_pass = passFile.readline()
-passFile.close()
+# passFile = open("pass.txt","r")
+# mysql_pass = passFile.readline()
+# passFile.close()
 
 class Predict():
     def __init__(self, isLocal):
-        self.db = mysql.connect(
-            host="localhost", user="root", passwd=mysql_pass, database="test")
-        self.cursor = self.db.cursor()
-        self.databaseName = "cameraDatabaseFinal"
+        # self.db = mysql.connect(
+        #     host="localhost", user="root", passwd=mysql_pass, database="test")
+        # self.cursor = self.db.cursor()
+        # self.databaseName = "cameraDatabaseFinal"
         if(isLocal):
             self.ctx = mx.gpu() if mx.context.num_gpus() else mx.cpu()
             self.net = self.get_model()
         self.image_extension=".png"
+        self.getDataJson()
 
-    def getNames(self):
-        self.db.reconnect()
-        query = """SELECT frameID
-                FROM """ + self.databaseName + """
-                WHERE process_flag=0
-                ORDER BY SUBSTRING(frameID, 7) 
-                LIMIT 6"""
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
+    # def getNames(self):
+    #     self.db.reconnect()
+    #     query = """SELECT frameID
+    #             FROM """ + self.databaseName + """
+    #             WHERE process_flag=0
+    #             ORDER BY SUBSTRING(frameID, 7) 
+    #             LIMIT 6"""
+    #     self.cursor.execute(query)
+    #     return self.cursor.fetchall()
     
     def read_pics_api(self, img_names):
         imgs = []
 
         for name in img_names:
-            im = Image.open("./FRAMES/" + name[0] + self.image_extension)
+            im = Image.open("./FRAMES/" + name + self.image_extension)
             im = (np.array(im)).reshape(256, 256, 3)
             imgs.append(im)
 
@@ -64,7 +66,7 @@ class Predict():
         for i in range(len(imgs)):
 
             dictionary = {}
-            dictionary['name'] = imageNames[i][0]
+            dictionary['name'] = imageNames[i]
 
             dictionary['data'] = [imgs[i]]
 
@@ -130,33 +132,117 @@ class Predict():
                     cords = bbox[i][k].tolist()
                     temp.append((int((cords[0]+cords[2])/2), int(cords[3])))
 
-            pred.update({img_names[i][0]: temp})
+            pred.update({img_names[i]: temp})
 
         return pred
 
-    def getStringFromPoints(self, points):
-        pointString = ''
-        for point in points:
-            mainString = str(point[0])+' '+str(point[1])
-            if(len(pointString) != 0):
-                pointString = pointString+','+mainString
-            else:
-                pointString = mainString
-        return pointString
+    def findWorldDistance(self,matrix, worldRatio, point1, point2):
+        point1 = np.asarray(point1).reshape((2, 1))
+        point2 = np.asarray(point2).reshape((2, 1))
 
-    def editDatabase(self, data):
-        for name, coords in data.items():
-            pointsString = self.getStringFromPoints(coords)
-            self.db.reconnect()
-            query = """UPDATE """+self.databaseName + """
-                    SET 
-                        coordinates=%s,
-                        process_flag=1
-                    WHERE 
-                        frameID=%s"""
-            values = (pointsString, name)
-            self.cursor.execute(query, values)
-        self.db.commit()
+        if(worldRatio==None):
+            return -1
+
+        temp = np.zeros((3, 1))
+        temp[0][0] = point1[0][0]
+        temp[1][0] = point1[1][0]
+        temp[2][0] = 1
+
+        cornerInTopView = matrix.dot(temp)
+        corner1 = np.zeros((2, 1))
+        corner1[0][0] = cornerInTopView[0][0] / cornerInTopView[2][0]
+        corner1[1][0] = cornerInTopView[1][0] / cornerInTopView[2][0]
+
+        temp = np.zeros((3, 1))
+        temp[0][0] = point2[0][0]
+        temp[1][0] = point2[1][0]
+        temp[2][0] = 1
+
+        cornerInTopView = matrix.dot(temp)
+        corner2 = np.zeros((2, 1))
+        corner2[0][0] = cornerInTopView[0][0] / cornerInTopView[2][0]
+        corner2[1][0] = cornerInTopView[1][0] / cornerInTopView[2][0]
+
+        if (worldRatio!=None):
+            pixelDistance = np.sqrt(
+                ((corner1[0][0] - corner2[0][0]) ** 2) + ((corner1[1][0] - corner2[1][0]) ** 2))
+            worldDistance = worldRatio * pixelDistance
+            return worldDistance
+        else:
+            return -1
+        
+    def getDataJson(self):
+        with open('cameraDatabase.json','r') as jsonFile:
+            cameraData=json.load(jsonFile)
+
+        self.cameraDataProcessed= {}
+
+        for cameraKey, cameraValue in cameraData.items():
+            if(cameraValue["CalibrationData"]!=None):
+                cameraMatrix= np.array(cameraValue["CalibrationData"]["calibrationMatrix"])
+                self.cameraDataProcessed.update({
+                    cameraKey:{
+                        "calibrationMatrix": cameraMatrix,
+                        "worldRatio": cameraValue["CalibrationData"]["worldRatio"]
+                    } 
+                })
+            else:
+                self.cameraDataProcessed.update({
+                    cameraKey:{
+                        "calibrationMatrix": None,
+                        "worldRatio": None
+                        }
+                })
+        
+    def processData(self,coordinates):
+        violatedPointsData={}
+            
+        for frameID, dbData in coordinates.items():
+            points=combinations(dbData,2)
+            violatedPoints=[]
+            for point1,point2 in points:
+                    distance=self.findWorldDistance(self.cameraDataProcessed[frameID[0:6]]["calibrationMatrix"],self.cameraDataProcessed[frameID[0:6]]["worldRatio"], point1, point2)
+                    if(point1 == point2):
+                        continue
+                    elif(distance == -1):
+                        continue
+                    elif(distance  < 150):
+                        violatedPoints.append((point1,point2))
+            violatedPointsData.update({
+                frameID: violatedPoints
+            })
+
+        # DELETE FILES
+        for frameID,violatedPoints in violatedPointsData.items():
+            if(len(violatedPoints)==0):
+                deleteFile=frameID+self.image_extension
+                os.remove('./FRAMES/'+deleteFile) 
+        
+        return violatedPointsData
+
+    # def getStringFromPoints(self, points):
+    #     pointString = ''
+    #     for point in points:
+    #         mainString = str(point[0])+' '+str(point[1])
+    #         if(len(pointString) != 0):
+    #             pointString = pointString+','+mainString
+    #         else:
+    #             pointString = mainString
+    #     return pointString
+
+    # def editDatabase(self, data):
+    #     for name, coords in data.items():
+    #         pointsString = self.getStringFromPoints(coords)
+    #         self.db.reconnect()
+    #         query = """UPDATE """+self.databaseName + """
+    #                 SET 
+    #                     coordinates=%s,
+    #                     process_flag=1
+    #                 WHERE 
+    #                     frameID=%s"""
+    #         values = (pointsString, name)
+    #         self.cursor.execute(query, values)
+    #     self.db.commit()
 
 
 # if __name__ == "__main__":
@@ -192,6 +278,8 @@ def startOnePrediction(imageNamesBuffer, lambda_number):
             model.editDatabase(prediction)
             print("LAMBDA "+str(lambda_number)+" PREDICTION")
             print(prediction)
+            violatedPoints=model.processData(prediction)
+            prediction(violatedPoints)
     # transformation.beginTransformation(updateIndex)
     # lockobject.release()
 
